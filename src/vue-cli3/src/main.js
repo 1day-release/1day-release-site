d7f64be9da81ef5b69c3eec5b219f0409f10bf80
ryoju-ohata <ryoju@example.com>
2018-11-24 18:01:41 +0900

    Support vue plugins

--- src/vue-cli3/src/main.js
+++ src/vue-cli3/src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueResource from 'vue-resource'
+import VueMoment from 'vue-moment'
+import VueAnalytics from 'vue-analytics'
+
+import 'reset-css'
+import 'source-code-pro/source-code-pro.css'
+
+Vue.use(VueResource)
+Vue.use(VueMoment)
+
 Vue.config.productionTip = false
 
 new Vue({
