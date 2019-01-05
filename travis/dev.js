ea28d0cb5262df0dc3bc2415399b24fd784ad80e
KoheiMayama <mym.kohei@gmail.com>
2019-01-05 23:41:31 +0900

    js追加

--- travis/dev.js
+++ travis/dev.js
@@ -1,27 +1,68 @@
-var AWS = require('aws-sdk')
-var fs = require('fs')
-var mime = require('mime-types')
+const fs = require('fs')
+const path = require('path')
+const async = require('async')
+const AWS = require('aws-sdk')
+const readdir = require('recursive-readdir')
 
-AWS.config.update({
+const { BUCKET, KEY, SECRET } = process.env
+const rootFolder = path.resolve(__dirname, './')
+const uploadFolder = './dist'
+const s3 = new AWS.S3({
+  signatureVersion: 'v4',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'ap-northeast-1'
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
-var dir = './dist'
-var s3 = new AWS.S3()
-fs.readdir(dir, function(err, files) {
-  if (err) throw err
-  files.filter(function(file) {
-    var params = {
-      Bucket: 'dev.1day-release.com',
-      Key: file,
-      Body: fs.readFileSync(dir + file),
-      ContentType: mime.lookup(file)
-    }
-    s3.putObject(params, function(err, data) {
-      if (err) console.log(err, err.stack)
-      else console.log(data)
-    })
+function getFiles(dirPath) {
+  return fs.existsSync(dirPath) ? readdir(dirPath) : []
+}
+
+async function deploy(upload) {
+  if (!BUCKET || !KEY || !SECRET) {
+    throw new Error('you must provide env. variables: [BUCKET, KEY, SECRET]')
+  }
+
+  const filesToUpload = await getFiles(path.resolve(__dirname, upload))
+
+  return new Promise((resolve, reject) => {
+    async.eachOfLimit(
+      filesToUpload,
+      10,
+      async.asyncify(async file => {
+        const Key = file.replace(`${rootFolder}/`, '')
+        console.log(`uploading: [${Key}]`)
+        return new Promise((res, rej) => {
+          s3.upload(
+            {
+              Key,
+              Bucket: 'dev.1day-release.com',
+              Body: fs.readFileSync(file)
+            },
+            err => {
+              if (err) {
+                return rej(new Error(err))
+              }
+              res({ result: true })
+            }
+          )
+        })
+      }),
+      err => {
+        if (err) {
+          return reject(new Error(err))
+        }
+        resolve({ result: true })
+      }
+    )
   })
-})
+}
+
+deploy(uploadFolder)
+  .then(() => {
+    console.log('task complete')
+    process.exit(0)
+  })
+  .catch(err => {
+    console.error(err.message)
+    process.exit(1)
+  })
