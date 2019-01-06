500196dbd193c798fa1026a598a5af20ec4c3a0e
KoheiMayama <mym.kohei@gmail.com>
2019-01-06 16:19:59 +0900

    stg,prd環境も共通モジュールに対応

--- travis/stg.js
+++ travis/stg.js
@@ -1,43 +1,4 @@
-var AWS = require('aws-sdk')
-var fs = require('fs')
-var mime = require('mime-types')
+var export_function = require('./put_to_s3')
 
-const bucket_name = 'stg.1day-release.com'
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'ap-northeast-1'
-})
-
-function put_to_s3(file) {
-  distination_path = file.replace('./dist/', '')
-  var params = {
-    Bucket: bucket_name,
-    Key: distination_path,
-    Body: fs.readFileSync(file),
-    ContentType: mime.lookup(file)
-  }
-
-  s3.putObject(params, function(err, data) {
-    if (err) console.log(err, err.stack)
-  })
-}
-
-var dir = './dist'
-var s3 = new AWS.S3()
-
-const dir_files_name = fs.readdirSync(dir)
-dir_files_name.forEach(dir_file => {
-  file_path = dir + '/' + dir_file
-  if (fs.statSync(file_path).isDirectory()) {
-    // ディレクトリの場合（第二階層までしか対応していない）
-    fs.readdirSync(file_path).forEach(deep_file => {
-      console.log(file_path + '/' + deep_file)
-      put_to_s3(file_path + '/' + deep_file)
-    })
-  } else {
-    // ファイルの場合
-    put_to_s3(file_path)
-  }
-})
+const s3_env_bucket_name = 'stg.1day-release.com'
+export_function.put_to_s3(s3_env_bucket_name)
